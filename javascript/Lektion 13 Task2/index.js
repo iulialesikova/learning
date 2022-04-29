44368084e15c01ee11b4cef91f02a5c81d41c0d2
Iuliia <yulia.lesikova@gmail.com>
2022-04-29 19:50:42 +0200

    Lektion13Task2

---
+++ javascript/Lektion 13 Task2/index.js
@@ -0,0 +1,47 @@
+function calculate() {
+    let a = prompt('Введите число 1');
+    let b = prompt('Введите число 2');
+    let result = sum(Number(a), Number(b));
+    alert(result);
+}
+
+
+function sum(a, b) {
+    return a + b;
+}
+
+function calculate1() {
+    let a = prompt('Введите число 1');
+    let b = prompt('Введите число 2');
+    let result = minus(Number(a), Number(b));
+    alert(result);
+}
+
+
+function minus(a, b) {
+    return a - b;
+}
+
+function calculate2() {
+    let a = prompt('Введите число 1');
+    let b = prompt('Введите число 2');
+    let result = multiply(Number(a), Number(b));
+    alert(result);
+}
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function calculate3() {
+    let a = prompt('Введите число 1');
+    let b = prompt('Введите число 2');
+    let result = division(Number(a), Number(b));
+    alert(result);
+}
+
+
+function division(a, b) {
+    return a / b;
+}
